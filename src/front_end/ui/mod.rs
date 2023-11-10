09af1d9f6b63d741558af9049f3f28f8105ae2bd
gertminov <jacobheim@web.de>
2023-11-10 21:45:02 +0100

    mehr module

---
+++ src/front_end/ui/mod.rs
@@ -0,0 +1,7 @@
+use crate::front_end::ui::ui::print_hallo;
+
+mod ui;
+
+pub fn print_hallo2() {
+    print_hallo()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+pub mod backend;
+pub mod front_end;
 fn main() {
+    front_end::ui::print_hallo2();
     println!("Hello, world!");
 }
