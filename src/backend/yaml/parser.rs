459f2233a423a6f6cf39f2325b7bedd2b434192b
gertminov <jacobheim@web.de>
2023-12-10 22:30:43 +0100

    lel

--- src/backend/yaml/parser.rs
+++ src/backend/yaml/parser.rs
@@ -3,14 +3,14 @@ use serde::Deserialize;
 use serde_yaml::{Mapping, Sequence, Value};
 use slab_tree::TreeBuilder;
 
-enum NodeType {
+enum NodeType <'a> {
     Children{name: String},
-    SingleSelect{options: [String], choice: String},
-    MultiSelect{options: [String], choices: [String]}
+    SingleSelect{options: Vec<String>, choice: String},
+    MultiSelect{options: Vec<String>, choices: Vec<String>}
 }
-impl NodeType {
-    pub const CHILDREN: String = String::from("children");
-    pub const OPTIONS: String = String::from("options");
+impl <'a> NodeType <'a> {
+    pub const CHILDREN: &'a str = "children";
+    pub const OPTIONS: & 'a str = "options";
 }
 pub fn parse_project_yaml(yaml_str: &str){
     let de = serde_yaml::Deserializer::from_str(yaml_str);
@@ -18,7 +18,7 @@ pub fn parse_project_yaml(yaml_str: &str){
     let mapping = dings.as_mapping().expect("No Mapping");
     let project = mapping.get("project").map(|p| p.as_mapping()).flatten().expect("No Project");
     let default_location = project.get("default_location").map(|l| l.as_str()).flatten();
-    TreeBuilder::new().with_root(default_location).build()
+    TreeBuilder::new().with_root(default_location).build();
     walk_project(project);
 }
 
